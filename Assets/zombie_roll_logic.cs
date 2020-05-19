8c408f77f248d8438a3384f9e66fa54008c51d2c
MAHMUD SYAKIRAN <1821054@chester.ac.uk>
2020-05-20 01:22:16 +0800

    Basic Unity3D physics game

---
+++ Assets/zombie_roll_logic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_roll_logic : MonoBehaviour
+{
+
+    Rigidbody m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
